89f4264d69a2cd1fe39d2750ddec59071d4c4522
Rico <ManuelRico98@outlook.es>
2021-01-01 18:32:44 -0600

    modificacion del modelo del user/jsonbackreference

--- src/main/java/com/api/streaming/StreamingApplication.java
+++ src/main/java/com/api/streaming/StreamingApplication.java
@@ -1,9 +1,13 @@
 package com.api.streaming;
 
+import com.api.streaming.config.StorageProperties;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class StreamingApplication {
 
 	public static void main(String[] args) {
